aaf4ce03977afff68dc56c40b4cdfc78ff2bc4a7
Robert Herbig <sheltim@gmail.com>
2017-04-26 23:20:36 -0400

    Update 'Water Towers'

--- Challenge 16 - Water Towers/rpherbig.rb
+++ Challenge 16 - Water Towers/rpherbig.rb
@@ -1,11 +1,30 @@
-input = [5, 2, 3, 2, 1, 3]
+input = [3, 1, 5, 1, 3, 1, 4, 1, 5]
 
-water_line = [input[0], input[-1]].min
+def calculate_water(heights)
+  left_index = 0
+  right_index = heights.length - 1
+  total = 0
+  max_left_index = 0
+  max_right_index = 0
+  while left_index <= right_index
+    if heights[left_index] <= heights[right_index]
+      if heights[left_index] >= max_left_index
+        max_left_index = heights[left_index]
+      else
+        total += max_left_index - heights[left_index]
+      end
+      left_index += 1
+    else
+      if heights[right_index] >= max_right_index
+        max_right_index = heights[right_index]
+      else
+        total += max_right_index - heights[right_index]
+      end
+      right_index -= 1
+    end
+  end
 
-water_collected = 0
-input[1..-2].each do |height|
-  current_water_level = water_line - height
-  water_collected += [current_water_level, 0].max
+  total
 end
 
-puts water_collected
+puts calculate_water(input)
